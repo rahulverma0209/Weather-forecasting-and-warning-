bd2f58d114c3d3a28faf6181ad0a301ad0821c25
RAHUL <35567176+rahulverma0209@users.noreply.github.com>
2019-04-04 23:38:38 +0530

    code optimised

--- src/sample/ResetAdminPassword.java
+++ src/sample/ResetAdminPassword.java
@@ -2,28 +2,18 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.*;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.Statement;
-import java.util.Properties;
-import java.util.ResourceBundle;
+import java.sql.*;
+import java.util.*;
 
 
 public class ResetAdminPassword implements Initializable {
 
     public static Integer otp=0;
-    String to ="rahul0512.pi@gmail.com";
-    String from = "amrita002010@gmail.com";
 
     @FXML
     public Button send_otp_btn,saveButton;
@@ -44,52 +34,6 @@ public class ResetAdminPassword implements Initializable {
 
         OTP o=new OTP();
         o.start();
-
-        /*int n1=(int)(Math.random()*100);
-        int n2=(int)(Math.random()*100);
-        int n3=(int)(Math.random()*100);
-
-        otp=(n1*10000)+(n2*100)+n3;
-
-        System.out.println(otp);
-
-
-        Properties properties = new Properties();
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.smtp.host", "smtp.gmail.com");
-        properties.put("mail.smtp.port", 587);
-        Session session = Session.getDefaultInstance(properties, new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("amrita002010@gmail.com", "RahuL@0209");
-            }
-
-        });
-        try {
-            MimeMessage message = new MimeMessage(session);
-
-            message.setFrom(new InternetAddress(from));
-
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
-            message.setSubject("OTP");
-
-            BodyPart messageBodyPart1 = new MimeBodyPart();
-            messageBodyPart1.setText(otp.toString());
-            Multipart multipartObject = new MimeMultipart();
-            multipartObject.addBodyPart(messageBodyPart1);
-
-            message.setContent(multipartObject);
-
-            Transport.send(message);
-            AlertBox.confirmation("Mail successfully sent");
-            System.out.println("Mail successfully sent");
-
-            send_otp_btn.setDisable(true);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
         send_otp_btn.setDisable(true);
     }
 
@@ -135,10 +79,11 @@ public class ResetAdminPassword implements Initializable {
 
 class OTP extends Thread{
 
-    String to ="rahul0512.pi@gmail.com";
-    String from = "amrita002010@gmail.com";
-
     public void run(){
+
+        String to ="rahul0512.pi@gmail.com";
+        String from = "amrita002010@gmail.com";
+
         int n1=(int)(Math.random()*100);
         int n2=(int)(Math.random()*100);
         int n3=(int)(Math.random()*100);
@@ -177,21 +122,9 @@ class OTP extends Thread{
 
             Transport.send(message);
 
-            /*Alert alert=new Alert(Alert.AlertType.CONFIRMATION);
-            alert.setHeaderText(null);
-            alert.setContentText("Mail successfully sent");
-            alert.show();*/
-
             System.out.println("Mail successfully sent");
 
-            //send_otp_btn.setDisable(true);
-
         } catch (Exception e) {
-
-            /*Alert alert=new Alert(Alert.AlertType.ERROR);
-            alert.setHeaderText(null);
-            alert.setContentText("Error in sending mail");
-            alert.show();*/
 
             e.printStackTrace();
         }
